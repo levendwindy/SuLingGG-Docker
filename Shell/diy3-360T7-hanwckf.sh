95a9b8b21f0240593adf34054614048e243f6e00
levenwindy <winyiday@qq.com>
2023-06-03 13:35:22 +0800

    Update diy3-360T7-hanwckf.sh

--- Shell/diy3-360T7-hanwckf.sh
+++ Shell/diy3-360T7-hanwckf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 ls -al
 ./REVISECONFIG.sh tc-tiny kmod-sched kmod-sched-cake kmod-sched-connmark kmod-sched-ipset
 echo '-----------------------------------------------'
